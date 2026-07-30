faf794c78cee844cd5f77c89a32208e21351dece
MW4NG1 <msage3803@gmail.com>
2026-07-30 14:11:57 +0300

    Update Cart component to receive cart items as props and display them

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 
-const Cart = () => {
+// This component recieves all item that are currently in the cart
+const Cart = ({ cart }) => {
   return (
     <div>
       <h2>Shopping Cart</h2>
       <ul>
         {/* TODO: Include items here in li tags with text 'ITEM.NAME is in your cart.' */}
+        {/* Display each item in the cart */}
+        {cart.map((item) => (
+          <li key={item.id}>
+            {item.name} is in your cart
+          </li>
+        ))}
       </ul>
     </div>
   )
