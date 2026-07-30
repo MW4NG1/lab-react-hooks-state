e0ba0070373526504aef1750079b4c51d278ed67
MW4NG1 <msage3803@gmail.com>
2026-07-30 13:59:57 +0300

    Fix variable casing in DarkModeToggle component and render Cart component in App

--- src/App.jsx
+++ src/App.jsx
@@ -55,6 +55,7 @@ const App = () => {
       />
 
       {/* TODO: Implement and render Cart component */}
+      <Cart cart={cart} />
     </div>
   )
 }
--- src/components/DarkModeToggle.jsx
+++ src/components/DarkModeToggle.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 // The component will receive the current dark mode state and a fucntion that changes the state
-const DarkModeToggle = ({ DarkMode, toggleDarkMode }) => {
+const DarkModeToggle = ({ darkMode, toggleDarkMode }) => {
   return (
     <button onClick={toggleDarkMode}>
-      {DarkMode ? "Toggle Light Mode" : "Toggle Dark Mode"}
+      {darkMode ? "Toggle Light Mode" : "Toggle Dark Mode"}
     </button>
   );
 };
